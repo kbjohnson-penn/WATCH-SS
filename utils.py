82ea3bfc6c5037666b3711fba82c20f8430efa2f
sfpugh <sfpugh>
2025-05-27 19:41:47 +0000

    updated utils to include llm info

--- utils.py
+++ utils.py
@@ -1,45 +1,22 @@
-import tiktoken
-
-def num_tokens_from_messages(messages, model):
-    """Return the number of tokens used by a list of messages."""
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        # print("Warning: model not found. Using o200k_base encoding.")
-        encoding = tiktoken.get_encoding("o200k_base")
-    if model in {
-        "gpt-3.5-turbo-0125",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        "gpt-4o-mini-2024-07-18",
-        "gpt-4o-2024-08-06"
-        }:
-        tokens_per_message = 3
-        tokens_per_name = 1
-    elif "gpt-3.5-turbo" in model:
-        # print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0125.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0125")
-    elif "gpt-4o-mini" in model:
-        # print("Warning: gpt-4o-mini may update over time. Returning num tokens assuming gpt-4o-mini-2024-07-18.")
-        return num_tokens_from_messages(messages, model="gpt-4o-mini-2024-07-18")
-    elif "gpt-4o" in model:
-        # print("Warning: gpt-4o and gpt-4o-mini may update over time. Returning num tokens assuming gpt-4o-2024-08-06.")
-        return num_tokens_from_messages(messages, model="gpt-4o-2024-08-06")
-    elif "gpt-4" in model:
-        # print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
-        return num_tokens_from_messages(messages, model="gpt-4-0613")
-    else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model {model}."""
-        )
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-    return num_tokens
+llms = {
+    "openai_gpt_4o": {
+        "model": "gpt-4o",
+        "tpm": 1e6,
+        "rpm": 6.15e3
+    },
+    "openai_gpt35_turbo_instruct": {
+        "model": "gpt-3.5-turbo",
+        "tpm": 6e4,
+        "rpm": 360
+    },
+    "meta_llama_v3_2_3b_instruct": {
+        "model": "",
+        "tpm": 6.54e5,
+        "rpm": None
+    },
+    "meta_llama_v3_1_8b_instruct": {
+        "model": "",
+        "tpm": 5.7e5,
+        "rpm": None
+    }
+}
