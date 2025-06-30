2b5051014419900a64dcebdb5c296404b853b714
sfpugh <sfpugh>
2025-06-30 18:34:19 +0000

    added function to create custom spacy nlp

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import re
+import spacy
+from spacy.language import Language
+from spacy.tokens import Token
+from spacy.tokenizer import Tokenizer
+
 llms = {
     "openai_gpt_4o": {
         "model": "gpt-4o",
@@ -20,3 +26,53 @@ llms = {
         "rpm": None
     }
 }
+
+
+def create_custom_nlp(model="en_core_web_md"):
+    '''
+    Load spaCy model and customize with special tokens
+    for transcripts. 
+
+    args:
+        model (str): spaCy mode name
+
+    return:
+        (spacy.lang): spaCy model
+    '''
+    # Register custom attributes
+    for tag_name in ["is_silence_tag", "is_inaudible_tag", "is_event_tag"]:
+        if not Token.has_extension(tag_name):
+            Token.set_extension(tag_name, default=False)
+
+    # Custom pipeline component to set transcript special tags
+    @Language.component("set_transcript_tags")
+    def set_transcript_tags_component(doc):
+        for token in doc:
+            # Handle silence tags
+            if re.match(r'\[silence\b.*\]', token.text):
+                token._.is_silence_tag = True
+            # Handle inaudible tags
+            elif token.text == "[inaudible]":
+                token._.is_inaudible_tag = True
+            # Handle all other event tags
+            elif re.match(r"^\[.*\]$", token.text):
+                token._.is_event_tag = True
+        return doc    
+
+    # Load the spaCy model
+    nlp = spacy.load(model)
+
+    # Customize the tokenizer to look for text brackets
+    nlp.tokenizer = Tokenizer(nlp.vocab, 
+                              rules=nlp.Defaults.tokenizer_exceptions,
+                              prefix_search=spacy.util.compile_prefix_regex(nlp.Defaults.prefixes).search,
+                              suffix_search=spacy.util.compile_suffix_regex(nlp.Defaults.suffixes).search,
+                              infix_finditer=spacy.util.compile_infix_regex(nlp.Defaults.infixes).finditer,
+                              token_match=re.compile(r"\[[^\]]+\]").match
+    )
+    
+    # Add custom tagging component to pipeline
+    nlp.add_pipe("set_transcript_tags", first=True)
+    
+    print(f"Updated pipeline: {nlp.pipe_names}")
+    return nlp
