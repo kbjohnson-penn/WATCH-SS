285d24c1005f16f69769b7f5e3a554780ca818ac
sfpugh <sfpugh>
2025-07-01 20:04:20 +0000

    reversing order of params

--- detectors/filler_speech/keyword_search.py
+++ detectors/filler_speech/keyword_search.py
@@ -1,4 +1,3 @@
-# import mlflow
 from ..common_detectors.keyword_detector import KeywordDetector
 # from .keywords_config import keywords
 
@@ -12,10 +11,9 @@ class FillerKeywordDetector(KeywordDetector):
             keywords (list) - list of keywords to detect
             flag_nonwords (bool) - flag nonwords as filler
         '''
-        super().__init__(keywords, nlp)
+        super().__init__(nlp, keywords)
         self.flag_nonwords = flag_nonwords
 
-    # @mlflow.trace
     def detect(self, text):
         '''
         Extends parent class detect method to also  
@@ -23,6 +21,6 @@ class FillerKeywordDetector(KeywordDetector):
         output, doc = super().detect(text, return_doc=True)
 
         if self.flag_nonwords:
-            output += [(token.idx, token.idx + len(token)) for token in doc if token.is_oov and not (token.is_punct or token.is_space or token._.is_silence_tag)]
+            output += [(token.idx, token.idx + len(token)) for token in doc if token.is_oov and not (token.is_punct or token.is_space or token._.is_silence_tag or token._.is_inaudible_tag or token._.is_event_tag)]
 
         return output
